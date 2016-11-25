b97735068f4964d35f314a9d2ebbf2366293e9b6
sumanmaity112 <suman.maity112@gmail.com>
2016-11-25 20:00:37 +0530

    Suman | Added ability to covert '!' to factorial code

--- src/toJs.js
+++ src/toJs.js
@@ -1,15 +1,20 @@
 var Parser = require("jison").Parser;
+var functions = require("../lib/src/symbolToFunction");
 
 var main = function (grammar, expressions, lookupTable) {
     var parser = new Parser(grammar);
     var parseTrees = parser.parse(expressions);
     var finalResult = parseTrees.map(function (tree) {
-        var jsCode = tree.toJs(lookupTable);
+        var jsCode = "";
+        jsCode += tree.toJs(lookupTable);
         if (tree.parent === undefined || tree.parent.type !== "Assignment") {
             if (jsCode.match(/^\s*var [A-Za-z0-9_]+\s*$/)) {
                 jsCode = jsCode + ";" + "\n" + "console.log(" + jsCode.split(" ")[1] + ")";
             } else
                 jsCode = "console.log(" + jsCode + ")";
+        }
+        if (tree.parent.type === "Factorial") {
+            jsCode = functions(tree.parent.toString()) + jsCode;
         }
         return jsCode + ";";
     });
